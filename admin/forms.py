02c8431900ae1920e9a364198a239f7e714b0c99
Alexander Andreev <carzil@yandex.ru>
2017-12-16 19:56:13 +0300

    admin update

--- admin/forms.py
+++ admin/forms.py
@@ -1,18 +1,42 @@
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import *
-from game.models import *
+from game.models import Location, LocationGroup, Activity
 
 
 class LocationForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
+    group_id = StringField("Group id")
     type = SelectField("Type", choices=Location.TYPES, coerce=int)
     description = StringField("Description", validators=[DataRequired()])
+
+    def __init__(self, *args, obj=None, **kwargs):
+        if obj is not None:
+            if obj.group:
+                kwargs["group_id"] = obj.group.id
+        super().__init__(*args, obj=obj, **kwargs)
+
+    def validate_group_id(self, field):
+        if not field.data:
+            return
+
+        try:
+            value = int(field.data)
+        except ValueError:
+            raise ValidationError("not int")
+        try:
+            LocationGroup.select().where(LocationGroup.id == value).get()
+        except:
+            raise ValidationError("group doesn't exists")
 
     def populate_obj(self, obj):
         obj.type = self.data["type"]
         obj.name = self.data["name"]
         obj.description = self.data["description"]
+        if self.data["group_id"]:
+            obj.group = LocationGroup.select().where(LocationGroup.id == int(self.data["group_id"]))
+        else:
+            obj.group = None
 
 
 class LocationGroupForm(FlaskForm):
@@ -24,3 +48,19 @@ class LocationGroupForm(FlaskForm):
         obj.type = self.data["type"]
         obj.name = self.data["name"]
         obj.description = self.data["description"]
+
+
+class ActivityForm(FlaskForm):
+    type = SelectField("Type", choices=Activity.TYPES, coerce=int)
+    duration = IntegerField("Duration")
+
+    def populate_obj(self, obj):
+        obj.type = self.data["type"]
+        obj.duration = self.data["duration"]
+
+
+class ItemForm(FlaskForm):
+    type = IntegerField("Type")
+
+    def populate_obj(self, obj):
+        obj.type = self.data["type"]
