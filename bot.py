04e4bd9ca02fc5b7faefc1f63f5a320ee1e43387
pixelindigo <m4s4n0bu@gmail.com>
2017-12-18 18:42:42 +0300

    clean registration

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 from envparse import env
 from enum import Enum, auto
+from functools import wraps
 from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, ConversationHandler, CommandHandler, MessageHandler, Filters
 from game.models import Hero, HeroState, HeroStateTransition, Location, LocationGateway
 from game.models import Mob, MobInstance
+from peewee import IntegrityError
 import logging
 
 # Enable logging
@@ -11,38 +13,38 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
 env.read_envfile()
 
+def registered(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        try:
+            hero = Hero.get(chat_id=update.effective_chat.id)
+        except Hero.DoesNotExist:
+            update.message.reply_text('You are not registered yet.\n'+
+                                        'Register with `/register %nickname%` command')
+            return
+        else:
+            return func(bot, update, hero, *args, **kwargs)
+    return wrapped
 
-class State:
-    REGISTER = auto()
-    INTERNAL = auto()
-
-def start(bot, update):
+def register(bot, update, args, job_queue):
+    nickname = args[0]
     try:
-        hero = Hero.get(chat_id=update.effective_chat.id)
-    except Hero.DoesNotExist:
-        update.message.reply_text(
-            'Greetings, friend!\n\n'+
-            'How would you like me to call you?')
-        return State.REGISTER
+        hero = Hero.create(name=nickname, hp_base=100,
+                    location=Location.get(type=Location.START),
+                    chat_id=update.effective_chat.id,
+                    state=HeroState.get(name='IDLE'))
+    except IntegrityError:
+        update.message.reply_text(f'Nickname {nickname} already exists')
     else:
-        update.message.reply_text(f'You are alredy registered, {hero.name}')
-        return State.INTERNAL
-
-def register(bot, update, job_queue):
-    update.message.reply_text(f'Welcome, {update.message.text}')
-    hero = Hero.create(name=update.message.text, hp_base=100,
-                location=Location.get(type=Location.START),
-                chat_id=update.effective_chat.id,
-                state=HeroState.get(name='IDLE'))
-    actions(bot, update, hero, job_queue)
-    return State.INTERNAL
+        update.message.reply_text(f'Welcome, {nickname}')
+        actions(bot, update, hero)
 
 available_actions = {Location.START: ["Travel"],
-                        Location.FIGHT: ["Fight", "Leave"]}
-def actions(bot, update, hero, job_queue):
+                        Location.FIGHT: ["Leave"]}
+
+def actions(bot, update, hero):
     replies = ReplyKeyboardMarkup([available_actions[hero.location.type]],
                                     one_time_keyboard=True,
                                     resize_keyboard=True)
@@ -53,11 +55,9 @@ def handle_actions(bot, update, hero, job_queue):
     query = update.message.text
     if query not in available_actions[hero.location.type]:
         update.message.reply_text(f"You can't do {query} from here")
-        return actions(bot, update, hero, job_queue)
+        return actions(bot, update, hero)
     if query == 'Travel' or query == 'Leave':
         return travel(bot, update, hero, job_queue)
-    elif query == 'Fight':
-        return fight(bot, update, hero, job_queue)
 
 
 def travel(bot, update, hero, job_queue):
@@ -83,12 +83,12 @@ def handle_travel(bot, update, hero, job_queue):
     hero.state = HeroState.get(name='IDLE')
     hero.save()
     if destination.type == Location.FIGHT:
-        job_queue.run_once(fight, 10, context=hero.id)
-    return actions(bot, update, hero, job_queue)
+        job_queue.run_once(fight, 5, context=hero.id)
+    return actions(bot, update, hero)
 
 def fight(bot, job):
     hero = Hero.get(id=job.context)
-    if hero.location.type != Location.FIGHT:
+    if hero.location.type != Location.FIGHT or hero.state.name =='FIGHT':
         return
     minotaur = Mob.get(name='Minotaur')
     mob = MobInstance.create(type=minotaur, hp=minotaur.hp)
@@ -108,7 +108,7 @@ def handle_fight(bot, update, hero, job_queue):
             update.message.reply_text(f"You killed {mob.type.name}")
             hero.state = HeroState.get(name='IDLE')
             hero.save()
-            return actions(bot, update, hero, job_queue)
+            return actions(bot, update, hero)
         mob.hp -= 50
         mob.save()
         update.message.reply_text(f"You hit {mob.type.name} with 50 dmg")
@@ -129,28 +129,41 @@ def handle_fight(bot, update, hero, job_queue):
     else:
         update.message.reply_text(f"Can't {action} now")
 
-def reactor(bot, update, job_queue):
-    try:
-        hero = Hero.get(chat_id=update.effective_chat.id)
-    except Hero.DoesNotExist:
-        return start(bot, update)
+@registered
+def cancel(bot, update, hero):
+    if hero.state.name == 'IDLE':
+        update.message.reply_text("There's nothing to cancel")
+    elif hero.state.name == 'FIGHT':
+        update.message.reply_text("Can't cancel a fight")
+    elif hero.state.name == 'TRAVEL':
+        hero.state = HeroState.get(name='IDLE')
+        hero.save()
+        return actions(bot, update, hero)
+
+
+@registered
+def show_inventory(bot, update, hero):
+    items = Hero.get(chat_id=update.effective_chat.id).items
+    listing = '\n'.join([f"{item.type.name}" for item in items])
+    if listing == '':
+        update.message.reply_text('Your inventory is empty')
+    else:
+        update.message.reply_text(listing)
+
+@registered
+def reactor(bot, update, hero, job_queue):
     handlers[hero.state.name](bot, update, hero, job_queue)
-    return State.INTERNAL
 
 handlers = {'IDLE': handle_actions,
             'TRAVEL': handle_travel,
             'FIGHT': handle_fight}
 
 updater = Updater(env("API_TOKEN"))
-conv_handler = ConversationHandler(entry_points=[CommandHandler('start', start),
-                MessageHandler(Filters.text, reactor, pass_job_queue=True)],
-        states={
-            State.REGISTER:[MessageHandler(Filters.text, register, pass_job_queue=True)],
-            State.INTERNAL:[MessageHandler(Filters.text, reactor, pass_job_queue=True)]
-            }, fallbacks=[])
-updater.dispatcher.add_handler(conv_handler)
-#updater.dispatcher.add_handler(CommandHandler('actions', actions))
-#updater.dispatcher.add_handler(MessageHandler(Filters.text, reactor))
+
+updater.dispatcher.add_handler(MessageHandler(Filters.text, reactor, pass_job_queue=True))
+updater.dispatcher.add_handler(CommandHandler('register', register, pass_args=True, pass_job_queue=True))
+updater.dispatcher.add_handler(CommandHandler('cancel', cancel))
+updater.dispatcher.add_handler(CommandHandler('inventory', show_inventory))
 
 updater.start_polling()
 updater.idle()
